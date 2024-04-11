b85b27a209f86e5519e9691ee4509489da229078
Derrick Chen <derrick.chen@coyote-x.com>
2024-04-10 23:47:14 -0400

    create simple pages for layout

---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,23 @@
+'use client'; // Client component
+
+import React from 'react'
+// import { useRouter, useParams, useSearchParams, usePathname } from 'next/navigation'
+
+const PropertyPage = () => {
+    // const router = useRouter();
+    // const { id } = useParams();
+    // const searchParams = useSearchParams();
+    // const name = searchParams.get('name');
+    // const pathName = usePathname();
+
+    return (
+        <div> 
+            {/* <button onClick={() => router.push('/')} className='bg-blue-500 p-2'>Go Home { id }</button> */}
+            {/* <button onClick={() => router.push('/')} className='bg-blue-500 p-2'>Go Home { name }</button> */}
+            {/* <button onClick={() => router.push('/')} className='bg-blue-500 p-2'>Go Home { pathName }</button> */}
+            PropertyPage
+        </div>
+    )
+}
+
+export default PropertyPage
