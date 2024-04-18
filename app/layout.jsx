1f938df87e044c61a2f47622f27d7a9f4a212292
Derrick Chen <derrick.chen@coyote-x.com>
2024-04-18 09:25:32 -0400

    use home page component on layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@/assets/styles/global.css'
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 export const metadata = {
     title: 'PropertyPulse | Find The Perfect Rental',
@@ -13,7 +14,8 @@ const MainLayout = ({ children }) => {
         <html lang='en'>
             <body>
                 <Navbar />
-                <main>{children}</main>
+                <main>{ children }</main>
+                <Footer />
             </body>
         </html>
     )
